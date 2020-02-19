157796433f2e2ccb95f56004ca4eb743c25fff8d
frankbouwens <5103a8425916473cd19e8fd6d4b7118c92aba882>
2020-02-19 13:42:31 +0100

    test

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const https = require('https');
+
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const webhook = core.getInput('webhook');
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+  console.log("github.context.issue.number: " + github.context.issue.number);
+  //POST Request
+  const data = JSON.stringify({
+    text: ""
+  });
+  //postRequest(webhook, data)
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(error.message)
+}
+
+function postRequest(webhook, data) {
+  const options = {
+    hostname: 'hooks.slack.com',
+    port: 443,
+    path: webhook.substring(23),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
+  };
+  const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+    req.on('error', err => {
+        console.log(err);
+        core.setFailed(err.message);
+      }
+    )
+  });
+  req.write(data);
+  req.end();
 }
